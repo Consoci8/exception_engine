11abd38e729e20c733130d68ec0f1a54be9aaaf9
Fadhli Rahim <fadhlirahim@gmail.com>
2011-02-10 12:53:18 +0800

    Added authentication hook and cleared the interface

--- app/controllers/exceptions_controller.rb
+++ app/controllers/exceptions_controller.rb
@@ -1,5 +1,7 @@
 class ExceptionsController < ApplicationController
+  before_filter :exception_engine_authentication
+  
   def index
-    @exceptions = ExceptionEngine::Data.all
+    @exceptions = ExceptionEngine::Data.all.descending(:created_at)
   end
 end
--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def exception_engine_authentication
+    # do nothing
+  end
 end
